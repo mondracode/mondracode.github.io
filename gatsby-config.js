326fd71f523f534ba99ccdfcaa0f016762cb9142
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2023-04-29 23:52:17 -0500

    add gatsby scaffolding

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `mondracode personal website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
